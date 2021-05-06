5c949c6a5272d23218b32850c09af62c68b02aba
nando <muhammad.rezki.ananda@gmail.com>
2021-05-06 10:48:25 +0700

    Add Shared Preferences

---
+++ app/src/main/java/com/submission/githubusersearch/storage/perferences/PreferencesConstant.kt
@@ -0,0 +1,3 @@
+package com.kotlinmvvm.cekongkir.database.preferences
+
+const val prefUsername = "pref_username"
---
+++ app/src/main/java/com/submission/githubusersearch/storage/perferences/PreferencesModel.kt
@@ -0,0 +1,3 @@
+package com.kotlinmvvm.cekongkir.database.preferences
+
+data class PreferencesModel(val type: String?, val id: String?, val name: String?)
